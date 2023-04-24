3eeac510dd33c15832b228f9fe689c3288394899
Hugo <hugo1798@hotmail.com>
2023-04-24 08:39:21 -0600

    Se agrega codigo comentado para uso de variables

---
+++ Programacion JAVA/ProgramaPrincipal/src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables{
+    public static void main(String[] args){
+        // declaracion e inicializacion de la variable de tipo local
+        int miNumero = 10;
+
+        System.out.println("Mi numero es: " + miNumero);
+        // al realizar la concatenacion, la variable de tipo entero 
+        //se convirtio en una variable de tipo string de manera automatica
+
+        // se pueden inicializar variables fuera del metodo main, 
+        //pero esto ya seria generar atributos para nuestra clase
+
+    }
+}
