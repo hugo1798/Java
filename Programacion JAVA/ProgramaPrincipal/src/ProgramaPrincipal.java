cf63cb6fb94a726be257022c5a89164317cd8315
Hugo <hugo1798@hotmail.com>
2023-04-24 08:04:30 -0600

    Se anade carpeta de trabajo con codigo base

---
+++ Programacion JAVA/ProgramaPrincipal/src/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+public class ProgramaPrincipal {
+    //public es una palabra reservada que significa que nuestra
+    //clase es publica, de manera que se puede acceder desde 
+    //cualquier otra clase, tambien permite que el sistema 
+    //ejecute lo que se coloque dentro de esta clase
+
+    public static void main(String[] args){
+        //Los modulos en java se conocen como paquetes
+        System.out.println("Hola mundo desde Java");
+    }
+    //el metodo main en este caso no regresa ningun valor, por 
+    //eso se utiliza void antes del metodo
+}
